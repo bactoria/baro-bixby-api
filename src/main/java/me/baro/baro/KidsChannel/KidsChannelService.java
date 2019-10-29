9d423b82a34e4ddcecb5cbc7e4ed9759a0f39c5e
bactoria <bactoria@gmail.com>
2019-10-30 08:13:01 +0900

    ADD :: 가이드 동영상의 videoId을 제공하는 기능 추가

--- src/main/java/me/baro/baro/KidsChannel/KidsChannelService.java
+++ src/main/java/me/baro/baro/KidsChannel/KidsChannelService.java
@@ -3,6 +3,7 @@ package me.baro.baro.KidsChannel;
 import lombok.RequiredArgsConstructor;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.jsoup.select.Elements;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
@@ -16,18 +17,18 @@ import java.util.List;
 
 @Component
 @RequiredArgsConstructor
-public class KidsChannelCrawl {
+public class KidsChannelService {
 
     private final KidsChannelRepository repository;
 
     public void crawlAll() {
-        final int PAGE_MAX_NUM = 10;
+        final int START_PAGE_NUM = 1;
+        final int END_PAGE_NUM = 10;
 
-        for (int pageNum = 1; pageNum <= PAGE_MAX_NUM; pageNum++) {
+        for (int pageNum = START_PAGE_NUM; pageNum <= END_PAGE_NUM; pageNum++) {
             crawl(pageNum);
         }
     }
-
 
     public void crawl(int pageNum) {
         Document doc = fetchDocument(pageNum);
@@ -41,7 +42,7 @@ public class KidsChannelCrawl {
         for (String re : strs[2].split("ci\":\"")) {
             String channelId = re.split("\"")[0];
             if (channelId.startsWith("U")) {
-                kidsChannels.add(new KidsChannel(channelId, null));
+                kidsChannels.add(new KidsChannel(channelId));
             }
         }
 
@@ -59,5 +60,32 @@ public class KidsChannelCrawl {
         }
 
         return doc;
+    }
+
+    public void updateKidsChannelInfo() {
+/*
+        List<KidsChannel> kidsChannels = new ArrayList<>();
+
+        for(String channelId : channelIds) {
+            final String targetUrl = "https://www.youtube.com/channel/" + channelId + "/about";
+
+            try {
+                Document doc = Jsoup.connect(targetUrl).header("Accept-language", "ko-KR").get();
+
+                Elements elements = doc.select("meta[property]");
+
+                String title = elements.select("meta[property=og:title]").attr("content");
+                String imageUrl = elements.select("meta[property=og:image]").attr("content");
+                String subscriber = doc.select("span.subscribed").attr("title");
+
+                KidsChannel kidsChannel = new KidsChannel(channelId, title, subscriber, imageUrl);
+                System.out.println(kidsChannel);
+                kidsChannels.add(kidsChannel);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return kidsChannels;
+*/
     }
 }
---
+++ src/main/java/me/baro/baro/PropertiesRestController.java
@@ -0,0 +1,23 @@
+package me.baro.baro;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author Bactoria
+ * @since 2019-10-29 [2019.10월.29]
+ */
+
+@RestController
+@RequestMapping("/property")
+public class PropertiesRestController {
+
+    @GetMapping("/guideVideoUrl")
+    public ResponseEntity<String> fetchGuideVideoId() {
+        String videoId = "eP4ga_fNm-E";
+        return ResponseEntity.ok().body(videoId);
+    }
+
+}
